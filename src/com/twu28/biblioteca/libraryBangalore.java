a880209ea1be1849e78129641c139a441656bb89
ashwinkonale <ashwin.konale@gmail.com>
2012-07-26 11:07:26 +0530

    Welcome Message Test Done

---
+++ src/com/twu28/biblioteca/libraryBangalore.java
@@ -0,0 +1,14 @@
+package com.twu28.biblioteca;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: SONY
+ * Date: 7/26/12
+ * Time: 11:02 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class libraryBangalore {
+    public String welcomemsg() {
+        return ("welcome to library");
+    }
+}
